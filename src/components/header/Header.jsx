dfefe0222907d84b6ad007148d07f4d349095c5e
Chanpech <70418825+Chanpech@users.noreply.github.com>
2024-06-22 17:09:08 -0400

    Update header image

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './header.css'
 import CTA from './CTA'
-import ME from '../../assets/prof_headshot1.png'
+import ME from '../../assets/graduatepic.png'
 import HeaderSocial from './HeaderSocial'
 
 //The style of naming classes is called  bim
